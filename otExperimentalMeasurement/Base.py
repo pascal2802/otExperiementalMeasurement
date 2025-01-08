3d3712fcadfca967f27a08c63491542956b4c183
Pascal Borel <pascal.borel@edf.fr>
2025-01-08 20:51:26 +0100

    firt version of PCE GLS

--- otExperimentalMeasurement/Base.py
+++ otExperimentalMeasurement/Base.py
@@ -1,4 +1,6 @@
 import openturns as ot
+import numpy as np
+import warnings
 
 
 class GeneralizedLeastSquareMethod:
@@ -51,7 +53,7 @@ class GeneralizedLeastSquareMethod:
         """
 
         if self.method == "SVD":
-            a, v=self._computeWithSVD(lambdaReg=lambdaReg)
+            a, v = self._computeWithSVD(lambdaReg=lambdaReg)
 
         return a, v
 
@@ -76,7 +78,7 @@ class GeneralizedLeastSquareMethod:
         # Compute SVD :
         GramGLS = self.GramGLS + ot.IdentityMatrix(self.size) * lambdaReg
         SigmaDiag, U, Vt = GramGLS.computeSVD(True)
-        Sigma_inv = ot.Matrix(self.size,self.size)
+        Sigma_inv = ot.Matrix(self.size, self.size)
         for i in range(self.size):
             Sigma_inv[i, i] = 1.0 / SigmaDiag[i]
 
@@ -84,9 +86,15 @@ class GeneralizedLeastSquareMethod:
 
         a = GramGLS_inv * self.design.transpose() * self.VY_inv * self.rhs
 
-        a = ot.Point([a[i,0] for i in range(self.size)])
+        a = ot.Point([a[i, 0] for i in range(self.size)])
 
         v = GramGLS_inv
+
+        try:
+            np.testing.assert_array_almost_equal(v, v.transpose())
+            v = ot.CovarianceMatrix(v)
+        except:
+            warnings.warn("Covariance Matrix of estimated coefficient is not symmetric")
 
         self.v = v
 
@@ -95,14 +103,149 @@ class GeneralizedLeastSquareMethod:
         return a, v
 
 
-class PCEWithGLS:
+class PCEWithGLSorGGMR:
     """
-    Create a PCE with GLS and provided Basis
+    Create a polynomial chaos by least squares with GLS or GLS-GGMR methods
 
     Parameters
     ----------
-    design : :class:`~openturns.Matrix`
-        a priori known design
+    inputSample : :class:`~openturns.Sample`
+        The input sample.
+    outputSample : :class:`~openturns.Sample`
+        The output sample with dimension 1
+    distribution : :class:`openturns.Distribution`
+        The distribution of the input.
+    VYCollection : sequence of :class:`openturns.CovarianceMatrix`
+        Collection of VarianceCovariance Matrix associated to each column of outputSample
+    multivariateBasis : :class:`~openturns.OrthogonalBasis`maxim
+        The orthogonal basis of functions.
+    totalDegree : int
+        Set the total degree of the PCE
+    maximumBasisSize : int
+        The maximum number of coefficients in the basis. if maximumBasisSize < basisSize(totalDegree), the n most significant coefficient are selected
+    selectedIndices : :class:`~openturns.IndicesCollection` - optional
+        indices of the basis functions used for the PCE - default None
+    leastSquaresMethod : str
+        The resolution method : "GLS" or "GGMR"
     """
-    def __init__(self):
-        x=1
+
+    def __init__(
+        self,
+        inputSample,
+        outputSample,
+        distribution,
+        VYCollection,
+        multivariateBasis,
+        totalDegree,
+        maximumBasisSize,
+        selectedIndices=None,
+        leastSquaresMethod="GLS",
+    ):
+        self.inputSample = inputSample
+        self.outputSample = outputSample
+        self.distribution = distribution
+        self.VYCollection = VYCollection
+        self.multivariateBasis = multivariateBasis
+        self.totalDegree = totalDegree
+        self.maximumBasisSize = maximumBasisSize
+        self.leastSquaresMethod = leastSquaresMethod
+        self.selectedIndices = selectedIndices
+        self.Transformation = ot.DistributionTransformation(
+            self.distribution, self.multivariateBasis.getMeasure()
+        )
+
+    def run(self):
+        """
+        Create the the functional chaos metamodel
+        """
+
+        enumerateFunction = self.multivariateBasis.getEnumerateFunction()
+        # Build an initial PC basis from the total degrees specified
+        if self.selectedIndices == None:
+            phisIndexes = list(
+                range(enumerateFunction.getMaximumDegreeCardinal(self.totalDegree))
+            )
+        else:
+            phisIndexes = self.selectedIndices
+        
+        phis = [
+            self.multivariateBasis.build(i)
+            for i in range(enumerateFunction.getMaximumDegreeCardinal(self.totalDegree))
+        ]
+        # Build the associated design matrix
+        proxy = ot.DesignProxy(self.Transformation(self.inputSample), phis)
+        design = proxy.computeDesign(
+            phisIndexes
+        )  # all provided basis functions are used to construct the design matrix
+
+        # Compute a_i for a full PCE
+        if self.leastSquaresMethod == "GLS":
+            # FIXME : generalisation with an outputSample with a dimension larger than 1 to be done
+            GLS = GeneralizedLeastSquareMethod(
+                design, self.outputSample[:, 0], self.VYCollection[0]
+            )
+            a, v = GLS.solve(lambdaReg=1e-5)
+
+        if a.getSize() > self.maximumBasisSize:
+            phisIndexes = ot.Indices(self.maximumBasisSize)
+            coeffSample = ot.Sample.BuildFromPoint(
+                a[1:]
+            )  # keep the a_0 coefficient in all cases
+            for i in range(coeffSample.getSize()):
+                coeffSample[i, 0] = abs(
+                    coeffSample[i, 0]
+                )  # take absolute value before sorting the coefficient
+            coeffRank = (
+                coeffSample.rank()
+            )  # compute the rank associated to each coefficient
+            phisIndexes[0] = 0  # Index associated to the mean (coefficient a_0)
+            CoeffIndex = ot.Sample.BuildFromPoint(
+                ot.Point(range(coeffSample.getSize()))
+            )
+            coeffRank.stack(CoeffIndex)
+            coeffRank = coeffRank.sort()
+            for i in range(
+                1, self.maximumBasisSize
+            ):  # only keep the coefficient with the highest value (part of variance explained)
+                phisIndexes[i] = int(coeffRank[-i, 1]) + 1
+
+            # compute a design matrix for the phis retained
+            design = proxy.computeDesign(phisIndexes)
+            # update functions collection for PCE result
+            phis = [self.multivariateBasis.build(i) for i in phisIndexes]
+
+            # update coefficient values
+            if self.leastSquaresMethod == "GLS":
+                # FIXME : generalisation with an outputSample with a dimension larger than 1 to be done
+                GLS = GeneralizedLeastSquareMethod(
+                    design, self.outputSample[:, 0], self.VYCollection[0]
+                )
+                a, v = GLS.solve(lambdaReg=1e-5)
+
+        residualsPoint = [1.0]
+        relativeErrorsPoint = [1.0]
+        self.result = ot.FunctionalChaosResult(
+            self.inputSample,
+            self.outputSample,
+            self.distribution,
+            self.Transformation,
+            self.Transformation.inverse(),
+            self.multivariateBasis,
+            phisIndexes,
+            ot.Sample.BuildFromPoint(a),
+            phis,
+            residualsPoint,
+            relativeErrorsPoint,
+        )
+
+    def getResult(self):
+        """
+        Return the functional chaos result.
+
+        Returns
+        -------
+        result : ot.FunctionalChaosResult
+            The metamodel.
+        """
+
+        return self.result
